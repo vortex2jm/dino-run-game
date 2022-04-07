439ce7d31c8ae56375bc9dab3db9cb718372576f
vortex2jm <joaopaulomoura5328@gmail.com>
2022-04-07 20:19:51 -0300

    reorganizing code

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,189 +1,176 @@
-import Phaser from '../lib/phaser.js'
+import Phaser from "../lib/phaser.js"
 
-export default class Game extends Phaser.Scene {
+export default class Game extends Phaser.Scene{
 
-    constructor() {
+    constructor(){
         super('game');
     }
 
-    init() {
+    init(){
 
-        //initializing preset vales
-        this.score = 0;
+        //presetting variables
         this.velocity = 1;
         this.velocity2 = 1;
-        this.obstacleExists = false;
+        this.score = 0;
         this.cactusVelocity = -400;
-        this.obstaclesArray = ['cactus', 'cactus2', 'cactus3'];
+        this.obstacleExists = false;
     }
 
-    preload() {
-        
-        //loading images
+    preload(){
+
+        //loading images and spritesheets
         this.load.image('background', './src/sprites/images/background2.png');
         this.load.image('platform', './src/sprites/images/gassPlatform.jpg');
         this.load.image('button', './src/sprites/images/playPause.png');
-        this.load.spritesheet('dino', './src/sprites/images/spritesheet.png', {frameWidth: 460, frameHeight: 410});
         this.load.image('cactus', './src/sprites/images/cactus1.png');
+        this.load.spritesheet('dino', './src/sprites/images/spritesheet.png', {
+            frameWidth: 460,
+            frameHeight: 410
+        });
 
-        //creating keys object
+        //creating keyboard object
         this.keys = this.input.keyboard.createCursorKeys();
     }
-    
-    create() {
 
-        
-        const {width , height} = this.scale;
-        const style = { color: '#000', fontSize: 24 };
+    create(){
 
-        //adding background
+        //standard data
+        const {width, height} = this.scale;
+        const style = {color: '#000', fontSize: 24};
+
+        //adding platform and background as a tilesprite
         this.background = this.add.tileSprite(width/2, height/2, width, height, 'background');
-
-        //adding dino
-        this.dino = this.physics.add.sprite(200, height/2, 'dino').setScale(0.3);
-        
-        //adding plattform
         this.platform = this.add.tileSprite(width/2, 400, width, 80, 'platform');
         this.physics.add.existing(this.platform, true);
-        
-        //adding colision
+
+        //adding dino as spritesheet
+        this.dino = this.physics.add.sprite(200, height/2, 'dino').setScale(0.3);
+
+        //adding physics
         this.physics.add.collider(this.dino, this.platform);
-        
+
         //adding text
-        this.scoreText = this.add.text(800, 50, 'Score: 0', style)
-            .setScrollFactor(0)
-            .setOrigin(0.5, 0);    
-        
+        this.scoreText = this.add.text(800, 50, 'Score: 0', style);
 
-        //adding buttons
-        const playPauseButton = this.add.image(85,70, 'button').setScale(0.03).setInteractive();
-        //animating and creating actions on buttons
-        playPauseButton.on('pointerover', () => {playPauseButton.setScale(0.04);})
-        playPauseButton.on('pointerout', () => {playPauseButton.setScale(0.03);})
-        playPauseButton.on('pointerdown', () => {
-
+        //adding actions to playPause button
+        const playPauseButton = this.add.image(85,70,'button').setScale(0.03).setInteractive();
+        playPauseButton.on('pointerover', ()=> {playPauseButton.setScale(0.04)});
+        playPauseButton.on('pointerout', ()=> {playPauseButton.setScale(0.03)});
+        playPauseButton.on('pointerdown', ()=> {
             this.scene.launch('pause');
             this.scene.pause();
         })
-        
 
-        //creating dino animations (spritesheet)
+        //creating animations to dino
         this.anims.create({
-
-            key: "run",
+            key: 'run',
             frames: this.anims.generateFrameNumbers('dino', {start: 0, end: 7}),
             frameRate: 12,
             repeat: -1
         })
         this.anims.create({
-
-            key: "jump",
-            frames: this.anims.generateFrameNumbers('dino', {start: 8, end: 19}),
-            frameRate: 15,
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('dino', {start: 9, end: 19}),
+            frameRate: 12,
             repeat: 1
         })
 
-        //creating obstacle group
-        this.obstaclesGroup = this.add.group();
-        this.physics.add.collider(this.platform, this.obstaclesGroup);
-        this.physics.add.collider(this.obstaclesGroup, this.dino,() => {
+        //creating group that will receive cactus
+        this.obstacleGroup = this.add.group();
+        this.physics.add.collider(this.platform, this.obstacleGroup);
+        this.physics.add.collider(this.obstacleGroup, this.dino, ()=> {
             this.scene.stop();
-            this.scene.start('game_over', {score: this.score})}, undefined, this);
+            this.scene.start('game_over', {score: this.score});
+        }, undefined, this);
     }
 
-    update() {
+    update(){
 
-        //moving background
+        //creating background and platform animations
         this.background.tilePositionX = this.velocity * this.velocity2;
-        //moving platform
-        this.platform.tilePositionX = this.velocity * 2 * this.velocity2;
+        this.platform.tilePositionX = this.velocity * this.velocity2 * 2;
 
-
-        //animating dino
+        //creating keys objects
         const touchingDown = this.dino.body.touching.down;
         const kUp = this.keys.up.isDown;
         const kSpace = this.keys.space.isDown;
         const kShift = this.keys.shift.isDown;
 
+        //defining actions to keys
         if(touchingDown && (kUp || kSpace)){
 
             this.dino.setVelocityY(-600);
-            this.dino.anims.play("jump");
+            this.dino.anims.play('jump', true);
         }
+
         else if(touchingDown){
 
-            this.dino.anims.play("run", true);
+            this.dino.anims.play('run', true);
         }
 
-        //dino jumps when we click on mouse
-        this.input.on('pointerdown', () => {
-
-            this.dino.setVelocityY(-600);
-            this.dino.anims.play("jump");
-        })
-
-        //adding key yo pause the game
         if(kShift){
 
             this.scene.launch('pause');
             this.scene.pause();
         }
-        
-        //additional functions
-        this.updateVelo();
+
+        //additional funcions
+        this.updateVelocity();
         this.updateScore();
         this.updateGroup();
     }
 
-    
-    updateScore() { //updating score
+
+    //additional functions implementation
+    updateVelocity(){
+
+        //updating velocities
+        this.velocity += 3;
+        this.velocity2 += 0.0001;
+        this.cactusVelocity -= 0.1;
+    }
+
+    updateScore(){
+
+        //updating score
         this.score++;
         this.scoreText.text = `Score: ${this.score}`;
     }
 
-    updateVelo() {  //updating velocities
+    createCactus(posXadd){
 
-        this.velocity += 3;
-        this.velocity2 += 0.0001;
-        this.cactusVelocity += -0.1;
-    }
-
-    updateGroup() {
-        
-        if(!this.obstacleExists){
-
-            let posXadd = Phaser.Math.Between(0,2000); //valuue to add to X cacts position
-            let rand = Phaser.Math.Between(1,2);    //value to decide 1/2 cactus
-
-            if(rand == 1){  //create only one cactus
-
-                this.createCactus(posXadd); 
-            }
-            else if(rand == 2){ //create two cactus
-
-                this.createCactus(posXadd);
-                this.createCactus(posXadd + 60);   
-            }
-
-            //actualizing boolean
-            this.obstacleExists = true;  
-        }
-        else{
-            
-            if(this.obstacle.x < -250){
-
-                //actualizing boolean
-                this.obstacleExists = false;
-            }   
-        }
-    }
-
-    createCactus(posXadd){  //function to create and animate cactus
-
-        this.obstacle = this.physics.add.sprite(1000 + posXadd, 310, this.obstaclesArray[0]).setScale(0.7);
+        //creating cactus, adding animation and inclding to group
+        this.obstacle = this.physics.add.sprite(this.scale.width + posXadd, 310, 'cactus').setScale(0.7);
         this.obstacle.setImmovable(true);
         this.obstacle.body.setAllowGravity(false);
         this.obstacle.setVelocityX(this.cactusVelocity);
-        this.obstaclesGroup.add(this.obstacle); 
+        this.obstacleGroup.add(this.obstacle);
+    }
+
+    updateGroup(){
+
+        //creating conditions to insert the group in game
+        if(!this.obstacleExists){
+
+            let posXadd = Phaser.Math.Between(0,2000);
+            let rand = Phaser.Math.Between(1,2);
+
+            if(rand == 1){
+
+                this.createCactus(posXadd);
+            }
+            else if(rand == 2){
+
+                this.createCactus(posXadd);
+                this.createCactus(posXadd + 60);
+            }
+
+            this.obstacleExists = true;
+        }
+
+        else{
+
+            if(this.obstacle.x < -250) this.obstacleExists = false
+        }
     }
 }
--- src/scenes/Pause.js
+++ src/scenes/Pause.js
@@ -1,56 +1,56 @@
-import Phaser from '../lib/phaser.js'
+import Phaser from "../lib/phaser.js"
 
-export default class Pause extends Phaser.Scene {
+export default class Pause extends Phaser.Scene{
 
-    constructor() {
+    constructor(){
+
         super('pause');
     }
 
     init(){};
 
-    preload() {
-
-        //creating keys object
-        this.keys = this.input.keyboard.createCursorKeys();
+    preload(){
 
         //loading images
         this.load.image('background', './src/sprites/images/background2.png');
         this.load.image('home', './src/sprites/images/home.png');
-        this.load.image('restart','./src/sprites/images/restart.png');
+        this.load.image('restart', './src/sprites/images/restart.png');
+        this.load.image('button', './src/sprites/images/playPause.png');
+
+        //create keyboard object
+        this.keys = this.input.keyboard.createCursorKeys();
     }
 
-    create() {
+    create(){
+
+        //standard data
+        const {width, height} = this.scale;
 
         //adding background
-        this.add.image(500, 250, 'background');
+        this.add.image(width/2, height/2, 'background');
 
-        //creating play/pause button and his actions
-        const playPauseButton2 = this.add.image(500,250, 'button').setScale(0.1).setInteractive();
-
-        playPauseButton2.on('pointerover', () => {playPauseButton2.setScale(0.12)});
-        playPauseButton2.on('pointerout', () => {playPauseButton2.setScale(0.1)});
-        playPauseButton2.on('pointerdown', () => {
+        //creating buttons and your own actions
+        const playPauseButton = this.add.image(width/2, height/2,'button').setScale(0.1).setInteractive();
+        playPauseButton.on('pointerover', ()=> {playPauseButton.setScale(0.12)});
+        playPauseButton.on('pointerout', ()=> {playPauseButton.setScale(0.1)});
+        playPauseButton.on('pointerdown', ()=> {
             this.scene.resume('game');
             this.scene.stop();
         })
 
-        //creating buttons objects
-        const home = this.add.image(900,410,'home').setScale(0.15).setInteractive();
-        const restart = this.add.image(100,410,'restart').setScale(0.15).setInteractive();
-
-        //home button actions
-        home.on('pointerover', () => {home.setScale(0.22)});
-        home.on('pointerout', () => {home.setScale(0.15)});
-        home.on('pointerdown', () => {
+        const home = this.add.image(900,410, 'home').setScale(0.15).setInteractive();
+        home.on('pointerover', ()=> {home.setScale(0.22)});
+        home.on('pointerout', ()=> {home.setScale(0.15)});
+        home.on('pointerdown', ()=> {
             this.scene.stop('game');
             this.scene.stop('pause');
             this.scene.start('start');
-        });
+        })
 
-        //restart button actions
-        restart.on('pointerover', () => {restart.setScale(0.22);})
-        restart.on('pointerout', () => {restart.setScale(0.15);})
-        restart.on('pointerdown', () => {
+        const restart = this.add.image(100, 410, 'restart').setScale(0.15).setInteractive();
+        restart.on('pointerover', ()=> {restart.setScale(0.22)});
+        restart.on('pointerout', ()=> {restart.setScale(0.15)});
+        restart.on('pointerdown', ()=> {
             this.scene.stop('game');
             this.scene.stop('pause');
             this.scene.start('game');
@@ -59,7 +59,7 @@ export default class Pause extends Phaser.Scene {
 
     update(){
 
-        //SHIFT key unpause the game
+        //creating action to shift key
         const kShift = this.keys.shift.isDown;
 
         if(kShift){
